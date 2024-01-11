48c48cc3682796de604b9b3a2e1dfa10f8ec6cfc
jubeaz <pascal.auillans@gmail.com>
2024-01-11 02:47:25 +0100

    update

--- vagrants/scripts/fix_ip.ps1
+++ vagrants/scripts/fix_ip.ps1
@@ -1,6 +1,7 @@
 # vmware bug to set the ip
 # see : https://github.com/hashicorp/vagrant/issues/5000#issuecomment-258209286
 
-param ([String] $ip)
-
-netsh.exe int ip set address Ethernet1 static $ip 255.255.255.0 
+param ([String] $ip, [String] $mask, [String] $gw, [String] $ip_i, [String] $mask_i)
+netsh.exe int ipv4 set address "Ethernet 2" static $ip mask=$mask gateway=$gw 
+netsh.exe int ipv4 set address "Ethernet 3" static $ip_i mask=$mask_i 
+route add 0.0.0.0 mask 0.0.0.0 $gw
