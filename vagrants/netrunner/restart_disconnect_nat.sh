bd1516d48a111a5e3e51de76e727b75044b8a768
jubeaz <pascal.auillans@gmail.com>
2024-01-16 01:31:08 +0100

    update

--- vagrants/netrunner/restart_disconnect_nat.sh
+++ vagrants/netrunner/restart_disconnect_nat.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#vagrant halt
+vagrant halt
 for b in $(cat Vagrantfile  | grep bname: | cut -d'"' -f 2); do
     echo "vboxmanage modifyvm $b --cableconnected1 off"
     vboxmanage modifyvm $b --cableconnected1 off
 done
 
 for b in $(cat Vagrantfile  | grep bname: | cut -d'"' -f 2); do
-    echo "vboxmanage startvm nrunner_fw --type headless"
-    vboxmanage startvm nrunner_fw --type headless
+    echo "vboxmanage startvm $b --type headless"
+    vboxmanage startvm $b --type headless
 done
