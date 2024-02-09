3fec3412c027b6000973b7f6ad0495595fbe4e90
jubeaz <pascal.auillans@gmail.com>
2024-02-09 16:34:19 +0100

    update

--- vagrants/scripts/fix_ip.ps1
+++ vagrants/scripts/fix_ip.ps1
@@ -1,12 +1,10 @@
 # vmware bug to set the ip
 # see : https://github.com/hashicorp/vagrant/issues/5000#issuecomment-258209286
 
-param ([String] $ip, [String] $mask, [String] $gw, [String] $ip_p, [String] $mask_p)
- netsh.exe int ipv4 set address "Ethernet 2" static $ip mask=$mask gateway=$gw 
- if ($ip_p -ne "None"){
-	netsh.exe int ipv4 set address "Ethernet 3" static $ip_p mask=$mask_p 
-	# when only public network
-	# netsh.exe int ipv4 set address "Ethernet 3" static $ip_p mask=$mask_p gateway=$gw
-}
-route add 0.0.0.0 mask 0.0.0.0 $gw
+param ([String] $if_name, [String] $ip, [String] $mask, [String] $gw)
+	if ($gw -ne "None"){
+		netsh.exe int ipv4 set address $if_name static $ip mask=$mask gateway=$gw 
+	} else {
+		netsh.exe int ipv4 set address "Ethernet 2" static $ip mask=$mask
+	}
 
--- vagrants/scripts/fix_ip.sh
+++ vagrants/scripts/fix_ip.sh
@@ -9,4 +9,6 @@ sed -i '/Name=en.*/d' /etc/systemd/network/80-dhcp.network
 sed -i 's/Name=eth.*/Name=eth0/g' /etc/systemd/network/80-dhcp.network
 systemctl restart systemd-networkd
 echo "Gateway=$1" >>  /etc/systemd/network/eth1.network
-
+echo "[Route]" >>  /etc/systemd/network/eth1.network
+echo "Gateway=$1" >>  /etc/systemd/network/eth1.network
+systemctl enable systemd-networkd
