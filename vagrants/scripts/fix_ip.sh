cf5167f79df98bc946f06df1647a283eac96a729
jubeaz <pascal.auillans@gmail.com>
2024-02-10 13:08:32 +0100

    update

--- vagrants/scripts/fix_ip.sh
+++ vagrants/scripts/fix_ip.sh
@@ -7,8 +7,11 @@
 
 sed -i '/Name=en.*/d' /etc/systemd/network/80-dhcp.network
 sed -i 's/Name=eth.*/Name=eth0/g' /etc/systemd/network/80-dhcp.network
-systemctl restart systemd-networkd
-echo "Gateway=$1" >>  /etc/systemd/network/eth1.network
-echo "[Route]" >>  /etc/systemd/network/eth1.network
-echo "Gateway=$1" >>  /etc/systemd/network/eth1.network
+#echo '[Link]' >> /etc/systemd/network/80-dhcp.network
+#echo  'Unmanaged=yes' >> /etc/systemd/network/80-dhcp.network
+
+sed -i 's/24/16/g' /etc/systemd/network/eth1.network
+#echo "[Route]" >>  /etc/systemd/network/eth1.network
+#echo "Gateway=$1" >>  /etc/systemd/network/eth1.network
 systemctl enable systemd-networkd
+systemctl restart systemd-networkd
