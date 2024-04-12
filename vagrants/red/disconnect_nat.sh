b20445d8378e24321377d0cd4153ebdbd388ab4c
jubeaz <pascal.auillans@gmail.com>
2024-04-12 16:08:42 +0200

    update

--- vagrants/red/disconnect_nat.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for b in $(cat Vagrantfile  | grep bname: | cut -d'"' -f 2); do
-    echo "vboxmanage modifyvm $b --cableconnected1 off"
-    vboxmanage modifyvm $b --cableconnected1 off
-done
--- vagrants/red/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for b in $(cat Vagrantfile  | grep bname: | cut -d'"' -f 2); do
-    echo "vboxmanage startvm $b --type headless"
-    vboxmanage startvm $b --type headless
-done
